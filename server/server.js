16ff4c8309294f71c35706a104a588c6009614d2
durget thomas <thomas.durget3+github@gmail.com>
2023-07-19 21:23:33 +0200

    back: ajout de la route '/orders'

--- server/server.js
+++ server/server.js
@@ -1,65 +1,65 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const port = 3000;
+const cors = require("cors");
 
-const { Sequelize, DataTypes } = require('sequelize')
+const { Sequelize, DataTypes } = require("sequelize");
 const { Op } = require("sequelize");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // CORS CSRF
-app.use(cors())
+app.use(cors());
 
+const sequelize = require("./config");
 
-const sequelize = require('./config')
+app.get("/", async (req, res) => {
+  res.json("Hello Humman Booster Students!");
+});
 
-
-
-app.get('/', async (req, res) => {
-  res.json('Hello Humman Booster Students!')
-})
-
-
-
-app.get('/contact', async (req, res) => {
-  res.json('Ma page de contact')
-})
-
+app.get("/contact", async (req, res) => {
+  res.json("Ma page de contact");
+});
 
 // une route pour lister les adresses
-app.get('/addresses', async (req, res) => {
+app.get("/addresses", async (req, res) => {
   const [results, metadata] = await sequelize.query("SELECT * FROM addresses");
 
-  return res.json(results)
-})
+  return res.json(results);
+});
 
 // une route pour lister les links
-app.get('/links', async (req, res) => {
+app.get("/links", async (req, res) => {
   const [results, metadata] = await sequelize.query("SELECT * FROM links");
 
-  return res.json(results)
-})
+  return res.json(results);
+});
 
-
-app.post('/addresses', async (req, res) => {
-  
-  let zipcode = req.body.zipcode
-  let city =  req.body.city
+app.post("/addresses", async (req, res) => {
+  let zipcode = req.body.zipcode;
+  let city = req.body.city;
 
   const [results, metadata] = await sequelize.query(`
     INSERT INTO addresses (zipcode, city) VALUES ("${zipcode}", "${city}")
   `);
 
-  res.json(results)
-})
+  res.json(results);
+});
 
+app.get("/orders", async (req, res) => {
+  const limit = req.query.limit > 0 ? parseInt(req.query.limit) : 5;
+  const sortBy = "id";
+  const [results, metadata] = await sequelize.query(
+    `SELECT * FROM orders ORDER BY ${sortBy} DESC LIMIT ${limit}`
+  );
+  res.json(results);
+});
 
-// Route en POST 
+// Route en POST
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
