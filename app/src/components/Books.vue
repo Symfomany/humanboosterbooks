596eeb9c6d0d1cbed0a7061ea0ff28d4aa8b9b3e
Camille Janin <cjanin@stagiaire-humanbooster.com>
2023-07-20 17:02:14 +0200

    ok changements

--- app/src/components/Books.vue
+++ app/src/components/Books.vue
@@ -1,39 +1,44 @@
 <template>
      <div class="news">  
-        <h2>Nos nouveautés</h2>
-        {{ books.length}} books
+        <div class="tete"> 
+            <h2>Nos nouveautés</h2>
+            {{ books.length}} books
+        </div>
         
-        <div v-for="book in books" :key="books.id" class = "card custom-card">
-            <img src ="{{}}" alt="">
-            <div  >
-                <h3> {{book.title}} </h3>
-                <p> {{book.price}} €</p>
-                <p v-if="book.disponibility === 1"> Disponible </p>
-                <p v-else> Non disponible </p>
-            </div>
-           
-            <div>
-                <p> {{book.publication_date}} </p>
-                <p> <!--auteurs AVG ('price')--> </p>
-                <p> <!--édition --> </p>
-            </div>
+        <div class="d-flex flex-row ">
+            <div v-for="book in books" :key="books.id" class = "card custom-card " style="width: 300px">
+                <img src ="{{}}" alt="">
+                <div  >
+                    <h3> {{book.title}} </h3>
+                    <p v-if="book.disponibility === 1"> Disponible </p>
+                    <p v-else> Non disponible </p>
+                    <p class="price"> {{book.price}} €</p>
+                </div>
+            
+                <div>
+                    <p> {{book.publication_date}} </p>
+                    <p> <!--auteurs --> </p>
+                    <p> <!--édition --> </p>
+                </div>
 
-            <div>
-                <p>{{book.synopsis}} </p>
-            </div>  
+                <div class="desc">
+                    <p>{{book.synopsis}} </p>
+                </div>  
 
-            <div>
-                <p>  {{book.nb_page}} pages </p>
-                <p> {{book.weight}} kg </p>
-                <p> {{book.size}} cm </p>
-            </div>
+                <div class="infos">
+                    <p>  {{book.nb_page}} pages </p>
+                    <p> {{book.weight}} kg </p>
+                    <p> {{book.size}} cm </p>
+                </div>
 
-            <div>
-                <ul>
-                    <li> {{book.isbn}} </li>
-                    <li> {{book.lang}} </li>
-                    <li> {{book.type}} </li>
-                </ul>
+                <div class="list-infos">
+                    <h4> Informations supplémentaires : </h4>
+                    <ul>
+                        <li>ISBN : {{book.isbn}} </li>
+                        <li>Langue : {{book.lang}} </li>
+                        <li>Type : {{book.type}} </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
@@ -71,13 +76,47 @@ export default {
 
 .news {
     padding:10px;
-}    
-    
-    h3{
-        color: blue
-    }
+}   
 
-    .custom-card {
-        border-color : gray ;
-    }
+.tete {
+    padding-bottom : 30px;
+}
+
+.desc {
+    font-size : 14px;
+    text-align: justify;
+}  
+
+h3{
+    font-weight: bold;
+}
+
+.custom-card {
+    border-color : gray ;
+    padding-top : 10px;
+    padding-bottom : 10px; 
+    padding-right : 25px;
+    padding-left : 25px;
+}
+
+.infos{
+    display : flex; 
+    flex-direction : row;
+    justify-content : center;
+    gap : 120px;
+    padding-top : 10px;
+    background-color : gray;
+    color : white; 
+    font-weight: bold;
+}
+
+.price{
+    font-size : 20px; 
+    font-weight: bold;
+}
+
+.list-infos{
+    text-align: start;
+    padding-top : 10px;
+}
 </style>
