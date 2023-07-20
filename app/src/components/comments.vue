ce161f79f68e2c3fa98457c8adc0ce685d94d50f
Quentin Orias <adressefictive@gmail.com>
2023-07-20 11:31:44 +0200

    comments component creation

---
+++ app/src/components/comments.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+      <h3>Commentaires</h3>
+      <div v-if="comments.length" class="row">
+        <div
+          v-for="(comment, index) in comments"
+          :key="index"
+          class="card col-md-6 col-lg-3"
+        >
+          Utilisateur :{{ comment.users_id }}<br />
+          Livre : {{ comment.books_id }} <br />
+          Commentaire :{{ comment.content }}<br />
+          Nombre de vues :{{ comment.views }}<br />
+          Date de création :{{ comment.created_date }} <br />
+          Dernière mise à jour :{{ comment.updated_day_time }} <br />
+        </div>
+      </div>
+      <template v-else>
+        <p>No comments...</p>
+      </template>
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  export default {
+    name: "Comments",
+    data() {
+      return { comments: [] };
+    },
+    async created() {
+      const { data } = await axios.get(`http://localhost:3000/comments`);
+      this.comments = data;
+    },
+  };
+  </script>
