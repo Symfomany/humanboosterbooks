04115431bbaf1f4f32ead446f3ef70329ab84ee7
alv <adlv@test.com>
2023-07-21 10:56:40 +0200

    delete v2

--- app/src/views/Users.vue
+++ app/src/views/Users.vue
@@ -76,7 +76,7 @@
 
 
         
-        <table class="table table-striped table-dark">
+        <table class="table table-striped table-dark w-100">
           <thead>
             <tr>
               <th scope="col">id</th>
