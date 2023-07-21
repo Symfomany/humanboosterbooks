cc2ecc959d3e5fe61a34f68e18167b611384bdf2
Quentin Orias <adressefictive@gmail.com>
2023-07-21 15:51:34 +0200

    ok

--- server/server.js
+++ server/server.js
@@ -38,22 +38,9 @@ app.get("/links", async (req, res) => {
   return res.json(results);
 });
 
-
-
-// une route pour récupérer la vidéo
-app.get('/ma-video', async (req, res) => {
-  const [results, metadata] = await sequelize.query("SELECT video, title FROM videos WHERE id = 1");
-
-  return res.json(results[0])
-})
-
-
-
-
-app.post('/addresses', async (req, res) => {
-  
-  let zipcode = req.body.zipcode
-  let city =  req.body.city
+app.post("/addresses", async (req, res) => {
+  let zipcode = req.body.zipcode;
+  let city = req.body.city;
 
   const [results, metadata] = await sequelize.query(`
     INSERT INTO addresses (zipcode, city) VALUES ("${zipcode}", "${city}")
@@ -62,29 +49,46 @@ app.post('/addresses', async (req, res) => {
   res.json(results);
 });
 
-//une route pour les commentaires
-app.get("/comments", async (req, res) => {
-  const limit = req.query.limit > 0 ? parseInt(req.query.limit) : 10;
+app.get("/orders", async (req, res) => {
+  const limit = req.query.limit > 0 ? parseInt(req.query.limit) : 5;
   const sortBy = "id";
-  const[results, metadata] = await sequelize.query(
-      `SELECT c.*, u.lastname, u.firstname, b.title FROM comments c JOIN users u ON c.users_id = u.id JOIN books b ON c.books_id = b.id ORDER BY ${sortBy} Desc LIMIT ${limit}`);
-  const[count] = await sequelize.query(
-      `SELECT COUNT(id) AS nbCount FROM comments`
+  const [results, metadata] = await sequelize.query(
+    `SELECT * FROM orders ORDER BY ${sortBy} DESC LIMIT ${limit}`
   );
-  res.json({results,count});
-    });
-
-//route pour delete les commentaires
-app.post('/delete-comments', async (req, res) => {
-
-  let delete_comments = req.body.id
-
-  const [results, metadata] = await sequelize.query (
-      `DELETE FROM comments WHERE id = ${delete_comments}`
-  );
-  return res.json({success : true});
+  res.json(results);
 });
-// Route en POST 
+
+app.get("/orders/stats", async (req, res) => {
+  const [results, metadata] = await sequelize.query(
+    `SELECT COUNT(*) AS nb_orders_by_stats ,status FROM orders GROUP BY status;`
+  );
+  res.json(results);
+});
+app.post;
+
+app.post("/orders/:id", async (req, res) => {
+  const status = req.body.status;
+  let result = "";
+  if (
+    req.params.id > 0 &&
+    (status == "en cours" ||
+      status == "livré" ||
+      status == "non livré" ||
+      status == "traité")
+  ) {
+    const id = parseInt(req.params.id);
+    await sequelize.query(
+      `UPDATE orders SET status = "${status}" WHERE orders.id = ${id}`
+    );
+    result = "success";
+  } else {
+    result = "error, invalid url or parameters";
+  }
+
+  return res.json(result);
+});
+
+// Route en POST
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
